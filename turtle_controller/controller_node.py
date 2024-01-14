ce00739e3134921da863e7c9ea70c948d193e082
krzy3chu <krzysztof.witkowski.16@gmail.com>
2024-01-14 19:04:04 +0100

    Add angular velocity steering

--- turtle_controller/controller_node.py
+++ turtle_controller/controller_node.py
@@ -16,42 +16,50 @@ class ControllerNode(Node):
         super().__init__('controller_node')
         self._subscription = self.create_subscription(Image, 'image_raw', self.camera_callback, 10)
         self._publisher = self.create_publisher(Twist, '/cmd_vel', 10)
-        self._dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_250)
-        self._control_msg = Twist()
+        self._control_msg = Twist()  # message to send in publisher
+        self._dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_250)  # define aruco type
 
     def camera_callback(self, image_data):
         cv_image = CvBridge().imgmsg_to_cv2(image_data,"bgr8")
         image_height = cv_image.shape[0]
         image_width = cv_image.shape[1]
-        turtle_velocity = 0.0
+        turtle_linear = 0.0 # turtlebot linear velocity
+        turtle_angular = 0.0 # turtlebot angular velocity
 
         markerCorners, markerIds, rejectedCandidates = cv2.aruco.detectMarkers(cv_image, self._dictionary)
         if markerCorners:
+            # draw lines around aruco marker
             marker = np.squeeze(markerCorners).astype(int)
             cv2.polylines(cv_image,[marker],True,(0,0,255), 5)
+            # draw circle of aruco marker
             M = cv2.moments(marker)
             cx = int(M['m10']/M['m00'])
             cy = int(M['m01']/M['m00'])
             cv2.circle(cv_image,(cx, cy), 10, (255,0,0), 5)
-            turtle_velocity = ((image_height / 2) - cy) / 100.0
+            # calculate turtlebot velocity and angle
+            turtle_linear = ((image_height // 2) - cy) / 100.0
+            turtle_angular = - ((image_width // 2) - cx) / 100.0
 
-        cv2.line(cv_image, (0, int(image_height/2)), (image_width, int(image_height/2)), (0,0,0), 3)
+        # draw center lines
+        cv2.line(cv_image, (0, image_height//2), (image_width, image_height//2), (0,0,0), 3)
+        cv2.line(cv_image, (image_width//2, 0), (image_width//2, image_height), (0,0,0), 3)
+        # show processed camera image
         cv2.imshow("camera", cv_image)
         cv2.waitKey(1)
 
-        print(type(turtle_velocity))
-        self._control_msg.linear.x = turtle_velocity
+        # publish and log message
+        self._control_msg.linear.x = turtle_linear
+        self._control_msg.angular.z = turtle_angular
         self._publisher.publish(self._control_msg)
         self.get_logger().info('Publishing: "%s"' % self._control_msg)
-
 
 
 def main(args=None):
     rclpy.init(args=args)
 
+    # handle ros node
     controller_node = ControllerNode()
     controller_node.get_logger().info('Turtle controller node started')
-
     rclpy.spin(controller_node)
     controller_node.destroy_node()
     rclpy.shutdown()
